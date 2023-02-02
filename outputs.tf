91af5905af34900928b9edbd7b6ff95ae77ea778
John Spinella <jrspinella@users.noreply.github.com>
2023-02-02 14:52:29 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+###############
+# Outputs    ##
+###############
+
+# Add outputs for the resource
+# output "resource_group_name_example" {
+#  value       = azurerm_resource_group.main_rg.name
+#  description = "Resource group name"
+# }
