2913fc754c46943ff28c464d6334d58d6bf5351e
John Spinella <jrspinella@users.noreply.github.com>
2024-01-30 10:40:50 -0500

    Fixing Template

--- locals.naming.tf
+++ locals.naming.tf
@@ -5,5 +5,5 @@ locals {
 
   resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, module.mod_scaffold_rg.*.resource_group_name, [""]), 0)
   location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, module.mod_scaffold_rg.*.resource_group_location, [""]), 0)
-  example_custom_name = coalesce(var.example_custom_name, data.azurenoopsutils_resource_name.example_custom_name.result)
+  example_custom_name = coalesce(var.custom_resource_group_name, data.azurenoopsutils_resource_name.example_custom_name.result)
 }
